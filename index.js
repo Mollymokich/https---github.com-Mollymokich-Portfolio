76b00cbd1523e177bdf23e8c54d6083a9b9d6211
Mollymokich <mollymokich91@gmail>
2023-03-02 12:45:16 +0600

    build

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
-const navButton = document.getElementById("nav-btn");
-const navMenu = document.getElementById("nav");
+const buttons = document.querySelectorAll("nav button");
 
-navButton.addEventListener("click", function () {
-  const isOpen = navMenu.classList.contains("active");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const target = document.getElementById(button.dataset.target);
 
-  if (isOpen) {
-    navMenu.classList.remove("active");
-    navButtonImg.src = "./img/icons/nav-open.svg";
-    navButtonImg.alt = "Navigation button";
-  } else {
-    navMenu.classList.add("active");
-    navButtonImg.src = "./img/icons/nav-close.svg";
-    navButtonImg.alt = "Close navigation button";
+    const isVisible = target.style.display !== "none";
+
+    target.style.display = isVisible ? "none" : "block";
+  });
+});
+
+document.getElementById("item1").onclick = function () {
+  window.location.href = "https://www.instagram.com/mollymokich/";
+};
+
+document.getElementById("item2").onclick = function () {
+  window.location.href = "https://m.facebook.com/moldir.amantay/";
+};
+
+document.getElementById("item3").onclick = function () {
+  window.location.href = "https://mollymokich.github.io/Molly/";
+};
+
+var button = document.querySelector(".btn");
+
+var modal = document.querySelector("#myModal");
+
+var close = document.querySelector(".close");
+
+button.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+});
+
+var button = document.querySelector(".btn");
+
+var modal = document.querySelector("#myModal");
+
+var close = document.querySelector(".close");
+
+button.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+var form = document.querySelector("form");
+var messageContainer = document.querySelector("#message-container");
+
+form.addEventListener("submit", function () {
+  modal.style.display = "none";
+  var message = document.createElement("div");
+  message.textContent = "Ваше сообщение успешно отправлено!";
+  messageContainer.appendChild(message);
 });
